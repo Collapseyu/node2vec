b46674e5ef160d8d7fd3b574eff1ad056ff4633e
Collapseyu <719144252@qq.com>
2021-05-10 14:16:50 +0800

    add time function

--- node2vec_cora.py
+++ node2vec_cora.py
@@ -3,25 +3,24 @@ import pandas as pd
 import numpy as np
 import node2vec
 import argparse
+import time
 
 cora_address = 'D:/yuyicong/workspace/cora/cora/'
 cora_content = 'cora.content'
 cora_cite = 'cora.cites'
 def parse_args():
     parser = argparse.ArgumentParser(description="Run node2vec.")
-    parser.add_argument('--dimensions', type=int, default=128,
-                        help='Number of dimensions. Default is 128.')
 
     parser.add_argument('--walk-length', type=int, default=80,
                         help='Length of walk per source. Default is 80.')
 
-    parser.add_argument('--num-walks', type=int, default=10,
+    parser.add_argument('--num-walks', type=int, default=1,
                         help='Number of walks per source. Default is 10.')
 
     parser.add_argument('--window_size', type=int, default=5,
                         help='Context size for optimization. Default is 5.')
 
-    parser.add_argument('--iter', default=1, type=int,
+    parser.add_argument('--iter', default=10, type=int,
                         help='Number of epochs in SGD')
 
     parser.add_argument('--workers', type=int, default=8,
@@ -33,7 +32,7 @@ def parse_args():
     parser.add_argument('--q', type=float, default=1,
                         help='Inout hyperparameter. Default is 1.')
     
-    parser.add_argument('--embed-size',type=int,default=128,
+    parser.add_argument('--embed-size',type=int,default=64,
                         help='Number of embedding size. Default is 128.')
     
     parser.add_argument('--min-count',type=int,default=0,
@@ -48,6 +47,7 @@ def parse_args():
     return parser.parse_args()
 if __name__ == "__main__":
     args = parse_args()
+    
     cora_edge = pd.read_table(cora_address+cora_cite,sep='\t',names = ['src','dst'])
     G = nx.Graph()
     node_dict = {i:v for v,i in enumerate(set(np.append(cora_edge.src.values,cora_edge.dst.values)))}
@@ -59,7 +59,10 @@ if __name__ == "__main__":
     cora_edge_list = list(map(map_func,cora_edge_list))
 
     G.add_edges_from(cora_edge_list)
+    startTime = time.perf_counter()
     n2v = node2vec.Node2Vec(G,args.p,args.q)
     n2v.train(num_walks = args.num_walks ,walk_length = args.walk_length, embed_size = args.embed_size, 
               window_size = args.window_size, workers = args.workers, iter_num = args.iter, min_count = args.min_count,
              sg = args.sg, hs = args.hs)
+    endTime = time.perf_counter()
+    print('epoch {}: Dimension from {} to {} use {} s'.format(args.iter,len(node_dict),args.embed_size,endTime-startTime))
